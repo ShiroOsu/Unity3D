e47d1cc1db92e3bd5eab5d8a6668cd6bbb0e50ff
Niklas Granskog <granen.n@gmail.com>
2019-06-02 17:08:42 +0200

    Update CircleEditor.cs

--- EditorAssignment/CircleEditor.cs
+++ EditorAssignment/CircleEditor.cs
@@ -4,30 +4,66 @@ using UnityEngine;
 [CustomEditor(typeof(Circle))]
 public class CircleEditor : EditorWindow
 {
-    public Object source;
+    public GameObject gameObject;
     private LineRenderer lineRenderer;
 
-    private int sides;
+    private int segments;
+    private const int zero;
+    private float lineWidth;
     private float radius;
 
     [MenuItem("Window/CircleEditor")]
     public static void ShowWindow()
     {
-        EditorWindow.GetWindow(typeof(CircleEditor));
+        GetWindow<CircleEditor>("Circle Editor");
     }
 
     public void OnGUI()
     {
         GUILayout.Label("Circle settings", EditorStyles.boldLabel);
-        lineRenderer = (LineRenderer)EditorGUILayout.ObjectField(source, typeof(LineRenderer), true);
+        gameObject = (GameObject)EditorGUILayout.ObjectField(gameObject, typeof(GameObject), true);
+        
+        if (!gameObject.GetComponent<LineRenderer>())
+        {
+            gameObject.AddComponent<LineRenderer>();
+            lineRenderer = gameObject.GetComponent<LineRenderer>();
+            lineRenderer.loop = true;
+        }
+        else
+        {
+            if (!gameObject.GetComponent<LineRenderer>().loop)
+            {
+                lineRenderer.loop = true;
+            }
+        }
+        
+        EditorGUI.BeginChangeCheck();
 
-        sides = EditorGUILayout.IntSlider("Sides", sides, 10, 50);
+        segments = EditorGUILayout.IntSlider("Sides", segments, 10, 50);
         radius = EditorGUILayout.Slider("Radius", radius, 1f, 10f);
+
+        if (EditorGUI.EndChangeCheck())
+        {
+            CreateCircle();
+        }
     }
 
-    private void Update()
+    private void CreateCircle()
     {
-        lineRenderer.gameObject.GetComponent<Circle>().segments = sides;
-        lineRenderer.gameObject.GetComponent<Circle>().radius = radius;
+        float deltaTheta = (2f * Mathf.PI) / segments;
+        float theta = zero;
+
+        lineRenderer.positionCount = segments;
+        lineRenderer.widthMultiplier = lineWidth;
+
+        Vector3 startPosition = gameObject.transform.position;
+
+        for (int index = zero; index < segments; index++)
+        {
+            Vector3 nextPosition = new Vector3(radius * Mathf.Cos(theta), radius * Mathf.Sin(theta), 0f);
+            lineRenderer.SetPosition(index, startPosition + nextPosition);
+
+            theta += deltaTheta;
+        }
     }
 }
